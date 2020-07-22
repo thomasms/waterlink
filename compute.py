5ac063e14b21f966da8508546b5302e45b4f73cd
thomasms <stainer.tom@gmail.com>
2020-07-22 22:24:41 +0200

    Final cost implementation

--- compute.py
+++ compute.py
@@ -35,11 +35,6 @@ in the data.csv file.
 
 """
 
-# 30 m3 per year per household
-BASIC_LIMIT_PER_HOUSEHOLD = 30
-# 30 m3 per year per person in each household
-BASIC_LIMIT_PER_PERSON = 30
-
 df_rates = pd.read_csv("data.csv")
 
 # make an effective rate as the sum of all three rates per basic and comfort
@@ -59,7 +54,7 @@ def compute_days_between_dates(startdate, enddate, includeenddate=True):
         return (enddate - startdate).days
 
 
-def prorate_scale(startdate=None, enddate=None):
+def prorate_scale(startdate=None, enddate=None, naive=True):
     """
     This could be a decorator
     """
@@ -77,21 +72,39 @@ def prorate_scale(startdate=None, enddate=None):
     if ed is None:
         ed = date(sd.year, 12, 31)
 
-    scale = compute_days_between_dates(sd, ed) / compute_days_between_dates(
-        date(sd.year, 1, 1), date(sd.year, 12, 31)
+    denom = (
+        365
+        if naive
+        else compute_days_between_dates(date(sd.year, 1, 1), date(sd.year, 12, 31))
     )
+
+    scale = compute_days_between_dates(sd, ed) / denom
     return scale
 
 
-def compute_basic_threshold(startdate=None, enddate=None, we=3, dom=3):
+def compute_basic_threshold(
+    startdate=None,
+    enddate=None,
+    we=3,
+    dom=3,
+    basic_limit_household=30,
+    basic_limit_person=30,
+):
     """
     This must be done per year. 
     Since the threshold is pro rated if not started on the 1st January.
 
     Need to compute days between and rate based on this.
 
+    - 30 m3 per year per household - fixed value for all years
+    basic_limit_household = 30
+
+    - 30 m3 per year per person in each household - fixed value for all years
+    basic_limit_person = 30
+
     """
-    full_year_threshold = BASIC_LIMIT_PER_HOUSEHOLD * we + BASIC_LIMIT_PER_PERSON * dom
+
+    full_year_threshold = basic_limit_household * we + basic_limit_person * dom
     return full_year_threshold * prorate_scale(startdate=startdate, enddate=enddate)
 
 
@@ -119,61 +132,106 @@ def compute_fees(
     return fee, discount
 
 
-# some simple tests
-START_DATE = date(2019, 2, 14)
+def compute_total_cost(reading, data, df=df_rates):
+    """
+        algorithm outline:
+        - start date from last bill
+        - end date is last reading submitted for bill
+        - the reading is the difference from end date to start date
+        - work out the number of people per apartment per each year
+                + [(year, we, dom), (year, we, dom)] - should not exceed 2 years, 
+                since each bill covers roughly 365 days just doesn't start on Jan 1 always.
+                But we will implement it to allow any number of years. However, still
+                we consider that WE of DOM changes only on Jan 1st.
+        - split the reading linearly across years
+                For example if it has 321 days in 2019 and 49 days in 2020, then we work
+                out thresholds based on 321/365 for WE1 and DOM1 and then 49/365 for WE2 and DOM2
+                for each year and split it
 
-# strictly this should be 2020 but since this was a leap year and the bill
-# doesn't take this into account we ignore it here so that the numbers
-# match up exactly - they use 365 (naive).
-END_DATE = date(2019, 2, 18)
+        parameters
+        ----------
+        # m3 for the period above
+        reading = 442.936
 
-# m3 for the period above
-# it is odd since the bill has both 443 and 449, but 442.94 seems 
-# to match perfectly with amounts in bill
-READING = 442.94
+        data = [
+            {"start": START_DATE, "end": date(START_DATE.year, 12, 31), "we": 3, "dom": 3},
+            {"start": date(END_DATE.year, 1, 1), "end": END_DATE, "we": 3, "dom": 5},
+        ]
 
-entries = [
-    {"start": START_DATE, "end": date(START_DATE.year, 12, 31), "we": 3, "dom": 3},
-    {"start": date(END_DATE.year, 1, 1), "end": END_DATE, "we": 3, "dom": 5},
-]
-"""
- algorithm outline:
-   - start date from last bill
-   - end date is last reading submitted for bill
-   - the reading is the difference from end date to start date
-   - work out the number of people per apartment per each year
-        + [(year, we, dom), (year, we, dom)] - should not exceed 2 years, 
-           since each bill covers roughly 365 days just doesn't start on Jan 1 always.
-           But we will implement it to allow any number of years. However, still
-           we consider that WE of DOM changes only on Jan 1st.
-   - split the reading linearly across years
-        For example if it has 321 days in 2019 and 49 days in 2020, then we work
-        out thresholds based on 321/365 for WE1 and DOM1 and then 49/365 for WE2 and DOM2
-        for each year and split it
-
-"""
-
-for entry in entries:
-    scale = prorate_scale(startdate=entry["start"], enddate=entry["end"])
-    threshold = compute_basic_threshold(
-        startdate=entry["start"], enddate=entry["end"], we=entry["we"], dom=entry["dom"]
-    )
-    fees = [
-        compute_fees(
-            df_rates,
+        returns
+        -------
+        single value representing total cost in EUR
+    """
+    total_cost = 0
+    fees_index = range(1, 4)
+    for entry in data:
+        sdf = df[df["year"] == entry["start"].year]
+        scale = prorate_scale(startdate=entry["start"], enddate=entry["end"])
+        threshold = compute_basic_threshold(
             startdate=entry["start"],
             enddate=entry["end"],
             we=entry["we"],
             dom=entry["dom"],
-            fee_col=f"fee{i}_eur_per_year",
-            discount_col=f"discount{i}_eur_per_year",
         )
-        for i in range(1, 4)
-    ]
-    fees_with_discount = [f - d for f, d in fees]
+        fees = [
+            compute_fees(
+                df,
+                startdate=entry["start"],
+                enddate=entry["end"],
+                we=entry["we"],
+                dom=entry["dom"],
+                fee_col=f"fee{i}_eur_per_year",
+                discount_col=f"discount{i}_eur_per_year",
+            )
+            for i in fees_index
+        ]
+        fees_with_discount = [f - d for f, d in fees]
 
-    reading = READING * scale
-    comfort_amount = reading - threshold
+        scaled_reading = reading * scale
+        comfort_amount = scaled_reading - threshold
 
-    # print(threshold, np.sum(fees_with_discount))
-    print(threshold, comfort_amount, fees)
+        basic_costs = [
+            threshold * sdf[f"rate{i}_basic_eur_per_m3"].values[0] for i in fees_index
+        ]
+        comfort_costs = [
+            comfort_amount * sdf[f"rate{i}_comfort_eur_per_m3"].values[0]
+            for i in fees_index
+        ]
+
+        total = np.sum(basic_costs) + np.sum(comfort_costs) + np.sum(fees_with_discount)
+
+        # print(threshold, np.sum(fees_with_discount))
+        # print(f'{entry["start"].year}: {threshold}, {comfort_amount}')
+        # for i in range(3):
+        #     print(" * ", fees[i][0], fees[i][1], basic_costs[i], comfort_costs[i])
+        # print(total)
+        total_cost += total
+    return total_cost
+
+
+# m3 for the period above
+# it is odd since the bill has both 443 and 449, but 442.94 seems
+# to match perfectly with amounts in bill
+print(
+    compute_total_cost(
+        reading=442.936,
+        data=[
+            {"start": date(2019, 2, 14), "end": date(2019, 12, 31), "we": 3, "dom": 3},
+            {"start": date(2020, 1, 1), "end": date(2020, 2, 18), "we": 3, "dom": 5},
+        ],
+        df=df_rates,
+    )
+)
+
+# if they had registered the correct number of people
+print(
+    compute_total_cost(
+        reading=443,
+        data=[
+            {"start": date(2019, 2, 14), "end": date(2019, 12, 31), "we": 3, "dom": 5},
+            {"start": date(2020, 1, 1), "end": date(2020, 2, 18), "we": 3, "dom": 6},
+        ],
+        df=df_rates,
+    )
+)
+
