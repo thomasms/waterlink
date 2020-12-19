8f3104f0f5952ad0b07da4e120fb6bc5cc9c1aa1
thomasms <stainer.tom@gmail.com>
2020-12-19 20:59:03 +0100

    Added some basic tests before refactoring

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # waterlink
+
+## Belgian water cost estimator for WaterLink
+
+Most people don't realise how much water costs, and are unaware of the "comfort" threshold.
+Once you go above this threshold water usage is charged double, yes double, the amount, so this simple computer tells you how much it will cost you.
+
+It is important to realise (and this takes it into account):
+
+- Water rates change per year
+- The threshold depends on the number of people in the household
+- The threshold depends on the number of households (if they share a water meter)
---
+++ tests.py
@@ -0,0 +1,90 @@
+import unittest
+from datetime import date
+
+from compute import compute_total_cost
+
+
+class ComputeUnitTest(unittest.TestCase):
+    def test_case1(self):
+        result = compute_total_cost(
+            reading=442.936,
+            data=[
+                {
+                    "start": date(2019, 2, 14),
+                    "end": date(2019, 12, 31),
+                    "we": 3,
+                    "dom": 3,
+                },
+                {
+                    "start": date(2020, 1, 1),
+                    "end": date(2020, 2, 18),
+                    "we": 3,
+                    "dom": 5,
+                },
+            ],
+        )
+        expected = 2689.12
+        self.assertAlmostEqual(result, expected, places=2)
+
+    def test_case2(self):
+        result = compute_total_cost(
+            reading=443,
+            data=[
+                {
+                    "start": date(2019, 2, 14),
+                    "end": date(2019, 12, 31),
+                    "we": 3,
+                    "dom": 5,
+                },
+                {
+                    "start": date(2020, 1, 1),
+                    "end": date(2020, 2, 18),
+                    "we": 3,
+                    "dom": 6,
+                },
+            ],
+        )
+        expected = 2454.98
+        self.assertAlmostEqual(result, expected, places=2)
+
+    def test_case3(self):
+        result = compute_total_cost(
+            reading=160,
+            data=[
+                {
+                    "start": date(2019, 2, 14),
+                    "end": date(2019, 12, 31),
+                    "we": 3,
+                    "dom": 3,
+                },
+                {
+                    "start": date(2020, 1, 1),
+                    "end": date(2020, 2, 18),
+                    "we": 3,
+                    "dom": 3,
+                },
+            ],
+        )
+        expected = 805.25
+        self.assertAlmostEqual(result, expected, places=2)
+
+    def test_case4(self):
+        result = compute_total_cost(
+            reading=180,
+            data=[
+                {
+                    "start": date(2019, 2, 14),
+                    "end": date(2019, 12, 31),
+                    "we": 3,
+                    "dom": 3,
+                },
+                {
+                    "start": date(2020, 1, 1),
+                    "end": date(2020, 2, 18),
+                    "we": 3,
+                    "dom": 3,
+                },
+            ],
+        )
+        expected = 875.50
+        self.assertAlmostEqual(result, expected, places=2)
