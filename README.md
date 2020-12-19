b2bdb5b2afa719c4071bf8c132b97a570dc130b2
thomasms <stainer.tom@gmail.com>
2020-12-19 21:22:21 +0100

    Removed numpy and pandas dependency - not really needed here

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ It is important to realise (and this takes it into account):
 - Water rates change per year
 - The threshold depends on the number of people in the household
 - The threshold depends on the number of households (if they share a water meter)
+
+**we** or **WE** is the number of households to take into account. This is if you share a water meter, by default this is just 1.
+
+**dom** or **DOM** is the number of people to take into account (across all households).
+
+For example:
+
+- if you live alone and do not share a water meter with another household: we=1, dom=1
+- if you are a couple and do not share a water meter with another household: we=1, dom=2
+- if you are a family of four and do not share a water meter with another household: we=1, dom=4
+- if you are a family of four and share a water meter with another household with another couple: we=2, dom=6
+
+It is important also to realise that they only count someone if they are registered in the household on the 1st January. If someone moves in on the 2nd January (officially) then they will not count until the next 1st January. Similarly if someone leaves on the 2nd January (officially) they will still be counted until 31st December. This is very important because this changes your comfort threshold - normally 30m3 per person.
--- compute.py
+++ compute.py
@@ -1,6 +1,4 @@
 from datetime import date
-import numpy as np
-import pandas as pd
 
 """
 You are charged at two rates: basic and comfort.
@@ -25,7 +23,7 @@ Since (30 + (30*3))*3 = 360 m3.
 This is very important when it comes to the cost, since the comfort rate
 is double that of the basic rate.
 
-Unfortunatly the DOM (gedomicilieerde), or people per residence, is determined
+Unfortunately the DOM (gedomicilieerde), or people per residence, is determined
 from the 1st January. Therefore if someone moves out on 2 January and a family
 of 4 moves in on the same day (or next day) then the threshold is still at 
 180 m3 instead of 270 m3 for 364 days of the year.
@@ -35,47 +33,46 @@ in the data.csv file.
 
 """
 
-# df_rates = pd.read_csv("data.csv")
-df_rates = pd.DataFrame.from_dict(
-    {
-        "discount1_eur_per_year": {0: 10, 1: 10},
-        "discount2_eur_per_year": {0: 6, 1: 6},
-        "discount3_eur_per_year": {0: 4, 1: 4},
-        "fee1_eur_per_year": {0: 50, 1: 50},
-        "fee2_eur_per_year": {0: 30, 1: 30},
-        "fee3_eur_per_year": {0: 20, 1: 20},
-        "rate1_basic_eur_per_m3": {0: 1.4084, 1: 1.4139},
-        "rate1_comfort_eur_per_m3": {0: 2.8168, 1: 2.8278},
-        "rate2_basic_eur_per_m3": {0: 1.0606, 1: 1.0657},
-        "rate2_comfort_eur_per_m3": {0: 2.1212, 1: 2.1314},
-        "rate3_basic_eur_per_m3": {0: 0.9939, 1: 0.9977},
-        "rate3_comfort_eur_per_m3": {0: 1.9878, 1: 1.9954},
-        "year": {0: 2019, 1: 2020},
-    }
-)
+RATES = {
+    2019: {
+        "discount1_eur_per_year": 10,
+        "discount2_eur_per_year": 6,
+        "discount3_eur_per_year": 4,
+        "fee1_eur_per_year": 50,
+        "fee2_eur_per_year": 30,
+        "fee3_eur_per_year": 20,
+        "rate1_basic_eur_per_m3": 1.4084,
+        "rate1_comfort_eur_per_m3": 2.8168,
+        "rate2_basic_eur_per_m3": 1.0606,
+        "rate2_comfort_eur_per_m3": 2.1212,
+        "rate3_basic_eur_per_m3": 0.9939,
+        "rate3_comfort_eur_per_m3": 1.9878,
+    },
+    2020: {
+        "discount1_eur_per_year": 10,
+        "discount2_eur_per_year": 6,
+        "discount3_eur_per_year": 4,
+        "fee1_eur_per_year": 50,
+        "fee2_eur_per_year": 30,
+        "fee3_eur_per_year": 20,
+        "rate1_basic_eur_per_m3": 1.4139,
+        "rate1_comfort_eur_per_m3": 2.8278,
+        "rate2_basic_eur_per_m3": 1.0657,
+        "rate2_comfort_eur_per_m3": 2.1314,
+        "rate3_basic_eur_per_m3": 0.9977,
+        "rate3_comfort_eur_per_m3": 1.9954,
+    },
+}
 
 
-# make an effective rate as the sum of all three rates per basic and comfort
-def compute_effective_rate(df):
-    basic_cols = [
-        c for c in df.columns if c.startswith("rate") and c.endswith("basic_eur_per_m3")
-    ]
-    df["rate_basic_effective_eur_per_m3"] = df[basic_cols].sum(axis=1)
-    return df
-
-
-# inclusive
 def compute_days_between_dates(startdate, enddate, includeenddate=True):
+    days = (enddate - startdate).days
     if includeenddate:
-        return (enddate - startdate).days + 1
-    else:
-        return (enddate - startdate).days
+        days += 1
+    return days
 
 
 def prorate_scale(startdate=None, enddate=None, naive=True):
-    """
-    This could be a decorator
-    """
     # we have a problem...
     if startdate is None and enddate is None:
         raise RuntimeError("You have a problem.")
@@ -93,7 +90,9 @@ def prorate_scale(startdate=None, enddate=None, naive=True):
     denom = (
         365
         if naive
-        else compute_days_between_dates(date(sd.year, 1, 1), date(sd.year, 12, 31))
+        else compute_days_between_dates(
+            date(sd.year, 1, 1), date(sd.year, 12, 31)
+        )
     )
 
     scale = compute_days_between_dates(sd, ed) / denom
@@ -103,7 +102,7 @@ def prorate_scale(startdate=None, enddate=None, naive=True):
 def compute_basic_threshold(
     startdate=None,
     enddate=None,
-    we=3,
+    we=1,
     dom=3,
     basic_limit_household=30,
     basic_limit_person=30,
@@ -123,14 +122,16 @@ def compute_basic_threshold(
     """
 
     full_year_threshold = basic_limit_household * we + basic_limit_person * dom
-    return full_year_threshold * prorate_scale(startdate=startdate, enddate=enddate)
+    return full_year_threshold * prorate_scale(
+        startdate=startdate, enddate=enddate
+    )
 
 
 def compute_fees(
-    df,
+    rates,
     startdate=None,
     enddate=None,
-    we=3,
+    we=1,
     dom=3,
     fee_col="fee1_eur_per_year",
     discount_col="discount1_eur_per_year",
@@ -140,13 +141,12 @@ def compute_fees(
     """
     year = enddate.year if startdate is None else startdate.year
     scale = prorate_scale(startdate=startdate, enddate=enddate)
-    sdf = df[df["year"] == year]
-    fee = sdf[fee_col].values[0] * scale * we
-    discount = min(sdf[discount_col].values[0] * scale * dom, fee)
+    fee = rates[year][fee_col] * scale * we
+    discount = min(rates[year][discount_col] * scale * dom, fee)
     return fee, discount
 
 
-def compute_total_cost(reading, data, df=df_rates):
+def compute_total_cost(reading, data, rates=RATES):
     """
         algorithm outline:
         - start date from last bill
@@ -179,7 +179,7 @@ def compute_total_cost(reading, data, df=df_rates):
     total_cost = 0
     fees_index = range(1, 4)
     for entry in data:
-        sdf = df[df["year"] == entry["start"].year]
+        yearrates = rates[entry["start"].year]
         scale = prorate_scale(startdate=entry["start"], enddate=entry["end"])
         threshold = compute_basic_threshold(
             startdate=entry["start"],
@@ -190,14 +190,16 @@ def compute_total_cost(reading, data, df=df_rates):
 
         scaled_reading = reading * scale
         comfort_amount = max(scaled_reading - threshold, 0)
-        basicreading = scaled_reading if scaled_reading < threshold else threshold
+        basicreading = (
+            scaled_reading if scaled_reading < threshold else threshold
+        )
 
         fees_with_discount = []
         basic_costs = []
         comfort_costs = []
         for i in fees_index:
             f, d = compute_fees(
-                df,
+                rates,
                 startdate=entry["start"],
                 enddate=entry["end"],
                 we=entry["we"],
@@ -207,18 +209,13 @@ def compute_total_cost(reading, data, df=df_rates):
             )
             fees_with_discount.append(f - d)
             basic_costs.append(
-                basicreading * sdf[f"rate{i}_basic_eur_per_m3"].values[0]
+                basicreading * yearrates[f"rate{i}_basic_eur_per_m3"]
             )
             comfort_costs.append(
-                comfort_amount * sdf[f"rate{i}_comfort_eur_per_m3"].values[0]
+                comfort_amount * yearrates[f"rate{i}_comfort_eur_per_m3"]
             )
 
-        total = np.sum(basic_costs) + np.sum(comfort_costs) + np.sum(fees_with_discount)
+        total = sum(basic_costs) + sum(comfort_costs) + sum(fees_with_discount)
 
-        # print(threshold, np.sum(fees_with_discount))
-        # print(f'{entry["start"].year}: {threshold}, {comfort_amount}')
-        # for i in range(3):
-        #     print(" * ", fees[i][0], fees[i][1], basic_costs[i], comfort_costs[i])
-        # print(total)
         total_cost += total
     return total_cost
